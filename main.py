4f306badb2985013841f26430b637e71b1524d02
Henri Hatch <henrihatch@gmail.com>
2022-10-01 16:45:41 -0500

    Base template for main.py

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 # Hellball main file, houses main code.
 
+def generatePlayer():
+  # Generate a list of random numbers to create random players
+
+  # Should go as such:
+  # First Name
+  # Last Name
+  # Ethnicity
+  # Age
+  # Quirk ID
+  # Creativity
+  # Dexterity
+  # Perception
+  # Intelligence
+  # Adaptability
+  # Strength
+  # Patience
+  # Insight
+  # Nerves
+  # Favorite Pitching Style
+  # Favorite Batting Style
+
+
+  return 0
+
+def main():
+  print("Run this every game.")
